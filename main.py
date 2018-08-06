043eee8ec5736816c293c944781cd2a7e65f6da9
hadware <hadware@gmail.com>
2018-08-06 19:07:14 +0200

    Des bases pour l'interface graphiquennww

--- main.py
+++ main.py
@@ -5,6 +5,63 @@ import gbulb
 from gi.repository import Gtk
 
 
+class AdminWindow(Gtk.Window):
+
+    def __init__(self):
+        super().__init__(self, title="Manipulateuw")
+        self.set_border_width(10)
+
+        # setting up grid on which to position elements
+        self.grid = Gtk.Grid()
+        self.grid.set_column_homogeneous(True)
+        self.grid.set_row_homogeneous(True)
+        self.add(self.grid)
+
+        # setting up userlist
+        self.scrollable_treelist = Gtk.ScrolledWindow()
+        self.scrollable_treelist.set_vexpand(True)
+        self.grid.attach(self.scrollable_treelist, 0, 0, 8, 10)
+
+        # setting up buttons
+        self.buttons = {"ban": Gtk.Button("Banniw"),
+                        "slowban": Gtk.Button("Slowbanniw"),
+                        "trash": Gtk.Button("Cancèw"),
+                        "shadowban": Gtk.Button("Fantomutèw")}
+        self.grid.attach_next_to(self.buttons["ban"], self.scrollable_treelist, Gtk.PositionType.BOTTOM, 1, 1)
+        self.grid.attach_next_to(self.buttons["slowban"], self.buttons["ban"], Gtk.PositionType.RIGHT, 1, 1)
+        self.grid.attach_next_to(self.buttons["trash"], self.buttons["slowban"], Gtk.PositionType.RIGHT, 1, 1)
+        self.grid.attach_next_to(self.buttons["shadowban"], self.buttons["trash"], Gtk.PositionType.RIGHT, 1, 1)
+        handlers_mapping = {"ban": self.ban_handler,
+                            "slowban": self.slowban_handler,
+                            "trash": self.trash_handler,
+                            "shadowban": self.shadowban_handler}
+        for key, button in self.buttons.items():
+            button.connect("clicked", handlers_mapping[key])
+
+        self.show_all()
+
+    def get_currently_selected(self):
+        """Return the user_id of the user currently being selected"""
+        pass
+
+    def ban_handler(self, widget):
+        """Called on ban button press"""
+        pass
+
+    def slowban_handler(self, widget):
+        """Called on slowban button press"""
+        pass
+
+    def trash_handler(self, widget):
+        """Called on trash button press"""
+        pass
+
+    def shadowban_handler(self, widget):
+        """Called on shadowban button press"""
+        pass
+
+
+
 @asyncio.coroutine
 def counter(label):
     i = 0
